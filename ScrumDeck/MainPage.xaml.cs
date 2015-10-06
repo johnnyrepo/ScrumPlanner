0a790b8f5724937bfc0bd3d801679b027012195b
J B <eugeni777@yandex.ru>
2015-10-06 19:37:16 +0300

    MVVM first approach - ViewModel introduced

--- ScrumDeck/MainPage.xaml.cs
+++ ScrumDeck/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using ScrumDeck.Resources;
 using ScrumDeck.Item;
+using ScrumDeck.ViewModel;
 
 namespace ScrumDeck
 {
@@ -22,17 +23,19 @@ namespace ScrumDeck
         {
             InitializeComponent();
 
-            SetTShirtScale();
+            DataContext = new CardViewModel(new Card[] { card1, card2, card3, card4, card5, card6, card7});
+
+            ((CardViewModel) DataContext).SetTShirtScale();
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
 
-        private void changeScaleBtn_Click(object sender, EventArgs e)
+        private void ChangeScaleBtn_Click(object sender, EventArgs e)
         {
             if (IsFibonacci)
             {
-                SetTShirtScale();
+                ((CardViewModel) DataContext).SetTShirtScale();
 
                 ((ApplicationBarIconButton) sender).Text = "Fibonacci";
 
@@ -40,7 +43,7 @@ namespace ScrumDeck
             }
             else
             {
-                SetFibonacciScale();
+                ((CardViewModel) DataContext).SetFibonacciScale();
 
                 ((ApplicationBarIconButton)sender).Text = "T-Shirt size";
 
@@ -48,53 +51,9 @@ namespace ScrumDeck
             }
         }
 
-        /*
-         * XXS = 1
-         * XS = 2
-         * S = 3
-         * M = 5
-         * L = 8
-         * XL = 13
-         * XXL = 21
-         * */
-        private void SetTShirtScale()
+        private void OnCardSelection_changed(object sender, SelectionChangedEventArgs e)
         {
-            card1.text.Text = "XXS";
-            card2.text.Text = "XS";
-            card3.text.Text = "S";
-            card4.text.Text = "M";
-            card5.text.Text = "L";
-            card6.text.Text = "XL";
-            card7.text.Text = "XXL";
-
-            card1.text.FontSize /= 2;
-            card7.text.FontSize /= 2;
-        }
-
-
-        private void SetFibonacciScale()
-        {
-            card1.text.Text = "1";
-            card2.text.Text = "2";
-            card3.text.Text = "3";
-            card4.text.Text = "5";
-            card5.text.Text = "8";
-            card6.text.Text = "13";
-            card7.text.Text = "21";
-
-            card1.text.FontSize = card2.text.FontSize;
-            card7.text.FontSize = card2.text.FontSize;
-        }
-
-        private void ResetCards(object sender, SelectionChangedEventArgs e)
-        {
-            card1.Reset();
-            card2.Reset();
-            card3.Reset();
-            card4.Reset();
-            card5.Reset();
-            card6.Reset();
-            card7.Reset();
+            ((CardViewModel)DataContext).ResetCards();
         }
 
         // Sample code for building a localized ApplicationBar
---
+++ ScrumDeck/ViewModel/CardViewModel.cs
@@ -0,0 +1,76 @@
+﻿using ScrumDeck.Item;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrumDeck.ViewModel
+{
+    class CardViewModel : INotifyPropertyChanged
+    {
+        private Card[] cards;
+
+        public CardViewModel(Card[] cards)
+        {
+            this.cards = cards;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void ResetCards()
+        {
+            foreach (Card card in cards) {
+                card.Reset();
+            }
+        }
+
+        /*
+        * XXS = 1
+        * XS = 2
+        * S = 3
+        * M = 5
+        * L = 8
+        * XL = 13
+        * XXL = 21
+        * */
+        public void SetTShirtScale()
+        {
+            cards[0].text.Text = "XXS";
+            cards[1].text.Text = "XS";
+            cards[2].text.Text = "S";
+            cards[3].text.Text = "M";
+            cards[4].text.Text = "L";
+            cards[5].text.Text = "XL";
+            cards[6].text.Text = "XXL";
+
+            cards[0].text.FontSize /= 2;
+            cards[6].text.FontSize /= 2;
+        }
+
+
+        public void SetFibonacciScale()
+        {
+            cards[0].text.Text = "1";
+            cards[1].text.Text = "2";
+            cards[2].text.Text = "3";
+            cards[3].text.Text = "5";
+            cards[4].text.Text = "8";
+            cards[5].text.Text = "13";
+            cards[6].text.Text = "21";
+
+            cards[0].text.FontSize = cards[1].text.FontSize;
+            cards[6].text.FontSize = cards[1].text.FontSize;
+        }
+    }
+}
